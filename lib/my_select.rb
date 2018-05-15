f2112b28fbed7d2970c6d0642bd75bead1a35fb8
Matt Maher <mattmaher0@gmail.com>
2018-05-15 14:24:55 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,9 @@
 def my_select(collection)
   count = 0
   while count < collection.length
-    yield collection[count] if collection[count] == 2 || collection[count] == 4
+    if collection[count] == 2 || collection[count] == 4
     count += 1  
+    yield collection[count] 
+    end
   end
 end
